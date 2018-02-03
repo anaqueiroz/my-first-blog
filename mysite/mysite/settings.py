e7bcfed29ea29af2477a81a9ad00847bbd9614dc
anaqueiroz <anaiccci@hotmail.com>
2018-02-03 16:01:06 -0200

    fix alowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'if07#okq7kob8p_9fw5s3z3wv(#yg2rxxq)hbg+h3-*wy2k306'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'anaqueiroz.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'anaqueiroz.pythonanywhere.com']
 
 
 # Application definition
